26cd58175a7bc7d63d42b200ba5c35538f407cb6
Antoine07 <antoine.lucsko@wanadoo.fr>
2026-01-22 16:41:24 +0100

    q

--- Apps/tp-tanstack-router-quiz/client/src/components/QuizCard.jsx
+++ Apps/tp-tanstack-router-quiz/client/src/components/QuizCard.jsx
@@ -1,15 +1,52 @@
-export default function QuizCard(props) {
+import { useState } from "react";
 
-    // mettre la logique du quiz pour cocher la bonne réponse, pour savoir quelle est la bonne réponse vous  props.answerIndex 
+export default function QuizCard(props) {
+  const [selected, setSelected] = useState(null);
+  const [message, setMessage] = useState("");
+  const [active, setActive] = useState(true);
+
+  // mettre la logique du quiz pour cocher la bonne réponse, pour savoir quelle est la bonne réponse vous  props.answerIndex
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (selected === null) return;
+  
+    if (selected == props.answerIndex) {
+      setMessage("Bonne réponse");
+    } else {
+       setMessage("Mauvaise réponse");
+    }
+
+    setSelected(null);
+    setActive(false);
+  };
 
   return (
-    <article>
-      <h1 className="text-balance text-base font-semibold text-slate-900">{props.title}</h1>
-      <ul>
-        {props.choices.map((choice, i) => (
-          <li key={i}>{choice}</li>
-        ))}
-      </ul>
+    <article className="quiz">
+      <form onSubmit={handleSubmit}>
+        <h1 className="text-balance text-base font-semibold text-slate-900">
+          {props.title}
+        </h1>
+        {message && <p>{message}</p>}
+        <ul>
+          {props.choices.map((choice, i) => (
+            <li key={i}>
+              <label>
+                {choice}
+                <input
+                  type="radio"
+                  checked={i == selected}
+                  onChange={() => setSelected(i)}
+                />
+              </label>
+            </li>
+          ))}
+        </ul>
+        <button className="button-link" disabled={!active}  >
+          Répondre
+        </button>
+      </form>
     </article>
   );
 }
